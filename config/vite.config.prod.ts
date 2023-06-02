4932c4db321a2c95d8b86674a7824be028308ce2
puppetk-666 <1063771217@qq.com>
2023-06-02 11:41:58 +0800

    feat(cli): vite config setting

---
+++ config/vite.config.prod.ts
@@ -0,0 +1,28 @@
+import { mergeConfig } from 'vite';
+import baseConfig from './vite.config.base';
+import configCompressPlugin from './plugin/compress';
+import configVisualizerPlugin from './plugin/visualizer';
+import configImageminPlugin from './plugin/imagemin';
+
+export default mergeConfig(
+  {
+    mode: 'production',
+    plugins: [
+      configCompressPlugin('gzip'),
+      configVisualizerPlugin(),
+      configImageminPlugin(),
+    ],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            ant: ['ant-design-vue', '@ant-design-vue/pro-layout'],
+            vue: ['vue', 'vue-router', 'pinia', '@vueuse/core'],
+          },
+        },
+      },
+      chunkSizeWarningLimit: 2000,
+    },
+  },
+  baseConfig,
+);
