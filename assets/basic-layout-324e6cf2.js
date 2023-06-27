3bd93c0089f49344b77cd571aecf6ec13fd7291d
puppet-666 <45839282+puppet-666@users.noreply.github.com>
2023-06-27 03:32:50 +0000

    Deploying to gh-pages from @ puppet-666/vue-simple-admin-starter@1ed11b42e08612d77cac7d2d658634f06c6f4784 🚀

---
+++ assets/basic-layout-324e6cf2.js
@@ -0,0 +1,1 @@
+import{a as m}from"./index-2b7ab220.js";import"./vue-e8af1fb6.js";import"./ant-4a4c0667.js";export{m as default};
--- assets/basic-layout-93630f1c.js
+++
@@ -1,1 +0,0 @@
-import{a as m}from"./index-ddfdb396.js";import"./vue-e8af1fb6.js";import"./ant-4a4c0667.js";export{m as default};
