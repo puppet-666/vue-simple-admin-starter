5a089cf686b3b0526a4ac9fe6697aea669ca5458
Raines <raines@youban.org>
2023-07-18 16:20:29 +0800

    chore(antdv): antdv升级至4.x版本

--- config/vite.config.base.ts
+++ config/vite.config.base.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
-import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+import AntDesignVueResolver from './utils/antd-resolver';
 
 export default defineConfig({
   plugins: [
